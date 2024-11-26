66863e813dc2d67714ccbf51a59ae287b522d37c
fredaperez <200300593@ucaribe.edu.mx>
2024-11-26 18:40:54 -0500

    actualización

---
+++ front/src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+    const [profesores, setProfesores] = useState([]);
+
+    useEffect(() => {
+        fetchProfesores();
+    }, []);
+
+    const fetchProfesores = async () => {
+        try {
+            const response = await axios.get("http://localhost:5000/profesores");
+            setProfesores(response.data);
+        } catch (error) {
+            console.error("Error al cargar los profesores:", error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Lista de Profesores</h1>
+            <ul>
+                {profesores.map((profesor, index) => (
+                    <li key={index}>{profesor.nombre} {profesor.apellido}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default App;
--- neo_config.py
+++
@@ -1,4 +0,0 @@
-
-uri = "neo4j+s://cbbcbd82.databases.neo4j.io"
-user = "neo4j"
-password = "3O-Wpjp9DPxZg5TzgsNNR4V5-0DGGFJausRHLWVGvMY"
